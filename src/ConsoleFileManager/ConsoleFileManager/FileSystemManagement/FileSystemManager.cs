e6534f641162b83c98d5e4ed821000ab230d421c
Tarxos <ftarxos@gmail.com>
2021-03-21 00:21:26 +0300

    prototype structure of file system manager

---
+++ src/ConsoleFileManager/ConsoleFileManager/FileSystemManagement/FileSystemManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleFileManager.FileSystemManagement
+{
+    public class FileSystemManager
+    {
+        public void GetDirectoryStruct(string directory)
+        {
+
+        }
+
+        #region Get Info
+
+        public void GetInfoDirectory(string directory)
+        {
+
+        }
+
+        public void GetInfoFile(string filePath)
+        {
+
+        }
+
+        #endregion
+
+        #region Copy
+
+        public void CopyDirectory(string directory)
+        {
+
+        }
+
+        public void CopyDirectories(IEnumerable<string> directories)
+        {
+
+        }
+
+        public void CopyFile(string filePath)
+        {
+
+        }
+
+        public void CopyFiles(IEnumerable<string> files)
+        {
+
+        }
+
+        #endregion
+
+        #region Delete
+
+        public void DeleteDirectory(string directory)
+        {
+
+        }
+
+        public void DeleteDirectories(IEnumerable<string> directories)
+        {
+
+        }
+
+        public void DeleteFile(string filePath)
+        {
+
+        }
+
+        public void DeleteFiles(IEnumerable<string> files)
+        {
+
+        }
+
+        #endregion
+
+
+        public void Print()
+        {
+
+        }
+    }
+}
