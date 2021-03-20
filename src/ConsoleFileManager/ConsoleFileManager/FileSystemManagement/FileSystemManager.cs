319524172d5cce211d739f18feb8fc4c0e2f8518
Tarxos <ftarxos@gmail.com>
2021-03-21 02:01:24 +0300

    Info prototype, Map Extensions for DirectoryInfo and FileInfo, GetLength for Directory

--- src/ConsoleFileManager/ConsoleFileManager/FileSystemManagement/FileSystemManager.cs
+++ src/ConsoleFileManager/ConsoleFileManager/FileSystemManagement/FileSystemManager.cs
@@ -1,25 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace ConsoleFileManager.FileSystemManagement
 {
     public class FileSystemManager
     {
-        public void GetDirectoryStruct(string directory)
+        public IEnumerable<FileSystemInfo> GetDirectoryStruct(string directory)
         {
-
+            var info = new DirectoryInfo(directory);
+            foreach (var dir in info.GetDirectories(directory))
+                yield return dir;
+            foreach (var file in info.GetFiles(directory))
+                yield return file;
         }
 
         #region Get Info
 
-        public void GetInfoDirectory(string directory)
-        {
+        public Info GetInfoDirectory(string directory) => new DirectoryInfo(directory).ToInfo();
 
-        }
-
-        public void GetInfoFile(string filePath)
-        {
-
-        }
+        public Info GetInfoFile(string filePath) => new FileInfo(filePath).ToInfo();
 
         #endregion
 
@@ -75,6 +75,46 @@ namespace ConsoleFileManager.FileSystemManagement
         public void Print()
         {
 
+        }
+    }
+
+    public class Info
+    {
+        public FileSystemInfo SystemInfo { get; init; }
+        public bool IsFile { get; init; }
+        public FileAttributes Attributes => SystemInfo.Attributes;
+
+        public long Length =>
+            IsFile
+                ? ((FileInfo) SystemInfo).Length
+                : ((DirectoryInfo) SystemInfo).GetLength();
+
+        public DateTime CreationTime => SystemInfo.CreationTime;
+        public DateTime LastAccessTime => SystemInfo.LastAccessTime;
+        public DateTime LastWriteTime => SystemInfo.LastWriteTime;
+    }
+
+    public static class Extensions
+    {
+        public static Info ToInfo(this DirectoryInfo directory) => new() {IsFile = false, SystemInfo = directory};
+        public static Info ToInfo(this FileInfo file) => new() {IsFile = true, SystemInfo = file};
+
+        public static long GetLength(this DirectoryInfo directory)
+        {
+            long actualLength = 0;
+            var innerDirectories = directory.GetDirectories();
+            for (var i = 0; i < innerDirectories.Length; i++)
+            {
+                actualLength = actualLength + innerDirectories[i].GetLength();
+            }
+
+            var innerFiles = directory.GetFiles();
+            for (var i = 0; i < innerFiles.Length; i++)
+            {
+                actualLength = actualLength + innerFiles[i].Length;
+            }
+
+            return actualLength;
         }
     }
 }
