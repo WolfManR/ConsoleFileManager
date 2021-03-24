d84a045a3ff77616532777fe3ccca85b387cd8f8
Tarxos <ftarxos@gmail.com>
2021-03-24 23:17:08 +0300

    ExceptionsFactory

---
+++ src/ConsoleFileManager/ConsoleFileManager/ExceptionsFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleFileManager
+{
+    public static class ExceptionsFactory
+    {
+        public static Exception PathNotExist(string path, string propertyName) =>
+            new ArgumentException($"Path not exist {path}", propertyName);
+        public static Exception SamePathAlreadyExist(string path, string propertyName) =>
+            new ArgumentException($"Path already exist {path}", propertyName);
+    }
+}
