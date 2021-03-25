c6c444b587e753f8761735ef12d398907eb84b52
Tarxos <ftarxos@gmail.com>
2021-03-26 01:15:46 +0300

    remove parameterName from Path not exist exception for time

--- src/ConsoleFileManager/ConsoleFileManager/Infrastructure/Exceptions/ExceptionsFactory.cs
+++ src/ConsoleFileManager/ConsoleFileManager/Infrastructure/Exceptions/ExceptionsFactory.cs
@@ -4,8 +4,8 @@ namespace ConsoleFileManager.Infrastructure.Exceptions
 {
     public static class ExceptionsFactory
     {
-        public static Exception PathNotExist(string path, string propertyName) =>
-            new ArgumentException($"Path not exist {path}", propertyName);
+        public static Exception PathNotExist(string path) =>
+            new ArgumentException($"Path not exist {path}");
         public static Exception SamePathAlreadyExist(string path, string propertyName) =>
             new ArgumentException($"Path already exist {path}", propertyName);
 
