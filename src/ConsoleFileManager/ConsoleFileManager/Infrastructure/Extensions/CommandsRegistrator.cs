58ab8c5c48902aa4f11659c9726cdfb6cd6b11b6
Tarxos <ftarxos@gmail.com>
2021-03-25 23:31:53 +0300

    Commands registration extension

---
+++ src/ConsoleFileManager/ConsoleFileManager/Infrastructure/Extensions/CommandsRegistrator.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleFileManager.Infrastructure.Commands;
+
+namespace ConsoleFileManager.Infrastructure.Extensions
+{
+    public static class CommandsRegistrator
+    {
+        public static InputCommandsParser CommandsRegistration(this InputCommandsParser parser) => parser
+            .Register(new ListPathCommand())
+            .Register(new ExitCommand())
+        ;
+    }
+}
