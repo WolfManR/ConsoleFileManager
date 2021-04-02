b42a5af0afd235e6a15ff7a17f69a7d30d6b1d0c
Tarxos <ftarxos@gmail.com>
2021-04-03 02:30:26 +0300

    extend functionality of ContentPresenter

--- src/ConsoleFileManager/ConsoleFileManager/Render/Abstracts/ContentPresenter.cs
+++ src/ConsoleFileManager/ConsoleFileManager/Render/Abstracts/ContentPresenter.cs
@@ -4,12 +4,23 @@ namespace ConsoleFileManager.Render.Abstracts
 {
     public class ContentPresenter
     {
-        public string Content { get; set; }
+        protected virtual string PrepairContent(int width, object content)
+        {
+            var msg = content?.ToString();
+            if (msg is not null)
+            {
+                if(msg.Length < width)
+                    return msg + new string(' ', width - msg.Length);
+                if (msg.Length > width)
+                    return msg[..width];
+            }
+            return msg ?? new string(' ', width);
+        }
 
-        public void Print(int x, int y, int width)
+        public void Print(int x, int y, int width, object content)
         {
             Console.SetCursorPosition(x, y);
-            var toPrint = Content.Length < width ? Content : Content[..width];
+            var toPrint = PrepairContent(width,content);
             Console.Write(toPrint);
         }
     }
