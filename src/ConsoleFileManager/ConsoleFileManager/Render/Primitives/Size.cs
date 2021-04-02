0dad8743e9f17ea3d83612a4a98ce3547a86f16a
Tarxos <ftarxos@gmail.com>
2021-04-03 02:21:39 +0300

    add equality to Size

--- src/ConsoleFileManager/ConsoleFileManager/Render/Primitives/Size.cs
+++ src/ConsoleFileManager/ConsoleFileManager/Render/Primitives/Size.cs
@@ -1,6 +1,8 @@
-﻿namespace ConsoleFileManager.Render.Primitives
+﻿using System;
+
+namespace ConsoleFileManager.Render.Primitives
 {
-    public struct Size
+    public struct Size : IEquatable<Size>
     {
         public int Width { get; set; }
         public int Height { get; set; }
@@ -10,5 +12,16 @@
             Width = width;
             Height = height;
         }
+
+        public override bool Equals(object obj) => obj is Size size && Equals(size);
+
+        public bool Equals(Size other) =>
+            Width == other.Width &&
+            Height == other.Height;
+
+
+        public static bool operator ==(Size left, Size right) => left.Equals(right);
+
+        public static bool operator !=(Size left, Size right) => !(left == right);
     }
 }
