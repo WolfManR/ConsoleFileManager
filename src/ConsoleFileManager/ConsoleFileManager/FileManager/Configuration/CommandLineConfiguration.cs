60d1b5e07c6a8198282031ee3d20deb8e54b706d
Tarxos <ftarxos@gmail.com>
2021-04-09 00:33:52 +0300

    FileManager configuration

---
+++ src/ConsoleFileManager/ConsoleFileManager/FileManager/Configuration/CommandLineConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleFileManager.FileManager.Configuration
+{
+    public class CommandLineConfiguration
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+    }
+}
---
+++ src/ConsoleFileManager/ConsoleFileManager/FileManager/Configuration/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleFileManager.FileManager.Configuration
+{
+    public class Configuration
+    {
+        public string OpenedPath { get; set; } = "D:/demo";
+        public int WindowWidth { get; set; } = 160;
+        public int WindowHeight { get; set; } = 40;
+        public int ViewPageSize { get; set; } = 20;
+        public bool InputMode { get; set; } = true;
+
+        public Action OnClose { get; set; }
+
+        public CommandLineConfiguration CommandLineConfiguration { get; set; }
+    }
+}
