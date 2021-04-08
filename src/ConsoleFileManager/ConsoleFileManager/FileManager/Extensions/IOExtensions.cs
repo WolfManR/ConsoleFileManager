6aa326ec065647dda640062827b894830d959ada
Tarxos <ftarxos@gmail.com>
2021-04-09 00:57:42 +0300

    Info model, IO extensions

---
+++ src/ConsoleFileManager/ConsoleFileManager/FileManager/Extensions/IOExtensions.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleFileManager.FileManager.Models;
+using System.IO;
+
+namespace ConsoleFileManager.FileManager.Extensions
+{
+    // ReSharper disable once InconsistentNaming
+    public static class IOExtensions
+    {
+        public static Info ToInfo(this DirectoryInfo directory) =>
+            new() { IsFile = false, Name = directory.Name, Path = directory.ToString() };
+        public static Info ToInfo(this FileInfo file) =>
+            new() { IsFile = true, Name = file.Name, Path = file.ToString(), FileExtension = file.Extension };
+
+        public static long GetLength(this FileSystemInfo self) =>
+            self switch
+            {
+                FileInfo file           => file.Length,
+                DirectoryInfo directory => directory.GetLength(),
+                _                       => -1
+            };
+
+        public static long GetLength(this DirectoryInfo directory)
+        {
+            long actualLength = 0;
+            var innerDirectories = directory.GetDirectories();
+            for (var i = 0; i < innerDirectories.Length; i++)
+            {
+                actualLength = actualLength + innerDirectories[i].GetLength();
+            }
+
+            var innerFiles = directory.GetFiles();
+            for (var i = 0; i < innerFiles.Length; i++)
+            {
+                actualLength = actualLength + innerFiles[i].Length;
+            }
+
+            return actualLength;
+        }
+    }
+}
---
+++ src/ConsoleFileManager/ConsoleFileManager/FileManager/Models/Info.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleFileManager.FileManager.Models
+{
+    public class Info
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public bool IsFile { get; set; }
+        public string FileExtension { get; set; }
+
+
+        public override string ToString() => $"{Name}  {Path}  {IsFile}  {FileExtension}";
+    }
+}
